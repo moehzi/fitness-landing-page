d39dd5c0ef1adad17f4f0e0aade381b24fc17308
moehzi <faizi1234098@gmail.com>
2021-11-10 13:48:18 +0800

    docs: Initialization colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: "#25AB75",
+      ornament: "#FFCD00",
+      time: "#FFE642",
+      calories: "#FB1351",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
